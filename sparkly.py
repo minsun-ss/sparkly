2aad797832248ff17de839e2c5b2834aa919cceb
stuffofminsun <sungsh@umich.edu>
2019-11-27 19:41:06 -0500

    1) Minor fixes to clarify code and fix mistakes.

--- sparkly.py
+++ sparkly.py
@@ -16,12 +16,13 @@ def display(df, order=None, type='line', max_rows=7, max_cols=999):
     :return: an HTML representation of the dataframe
     '''
 
-    # python object handling doesn't make sense so imma gonna make a copy for now until i get a handle on this
+    # python object handling doesn't make sense so imma make a copy for now until i get a handle on this
     temp_df = df.copy()
+    # set up default ordering if none available
     if order is None:
-        order = range(df.shape[0])
+        order = range(temp_df.shape[0])
 
-    temp_list = []
+    new_column_names_list = []
     # Determines which columns are numeric - changes based on a multilevel column
     if temp_df.columns.nlevels == 1:
         is_num = temp_df.select_dtypes(include=[np.number]).columns.values
@@ -33,35 +34,37 @@ def display(df, order=None, type='line', max_rows=7, max_cols=999):
     for column_name, column_data in zip(lowest_level, range((len(temp_df.columns)))):
         if column_name in is_num:
             image = convert_fig(temp_df.iloc[:, column_data].values, order, type)
-            text = column_name + image
-            temp_list.append(text)
+            new_column_name = column_name + image
+            new_column_names_list.append(new_column_name)
         else:
-            temp_list.append(column_name)
+            new_column_names_list.append(column_name)
 
     # replace column headers to include svg chart and renderrrrrrr away
     if temp_df.columns.nlevels == 1:
-        temp_df.columns = temp_list
+        temp_df.columns = new_column_names_list
     else:
         # build a column level mapper
         keys = list(lowest_level)
-        values = temp_list
+        values = new_column_names_list
         mapper = dict(zip(keys, values))
         temp_df.rename(columns=mapper, level=-1, inplace=True)
     return HTML(temp_df.to_html(escape=False, max_rows=max_rows, max_cols=max_cols))
 
 
 def convert_fig(a_series, order, type):
-    ''' Maps a numeric Series as a matplotlib chart and returns a html encoded byte string.
+    ''' Maps a numeric Series as a matplotlib chart and returns svg string
 
     :param a_series: Series to map
-    :order: specified xaxis for ordering
+    :param order: specified xaxis for ordering
+    :param type: type of chart generated
+    :return svg representation of chart in utf-8
     '''
 
     # build tiny chart
     fig = plt.figure()
     fig.set_size_inches(3, .5)
     ax = fig.gca()
-    if type=='histogram':
+    if type == 'histogram':
         ax.hist(a_series[~np.isnan(a_series)], 20)  # 20 bins
     else:
         ax.plot(order, a_series)
